d2f5af85d9da9c4d74b9ccff4bc7ad43eac01523
Anton Rybochkin <anton.rybochkin@axibase.com>
2021-01-07 02:43:38 +0300

    Fixed warning. FutureWarning: Passing a negative integer is deprecated in version 1.0 and will not be supported in future version. Instead, use None to not limit the column width.                  pd.set_option('max_colwidth', -1)

--- atsd_client/services.py
+++ atsd_client/services.py
@@ -739,5 +739,5 @@ def response_to_dataframe(resp, reserved, **frame_params):
         enc_resp.append(el)
     import pandas as pd
     pd.set_option("display.expand_frame_repr", False)
-    pd.set_option('max_colwidth', -1)
+    pd.set_option('max_colwidth', None)
     return pd.DataFrame(enc_resp, **frame_params)
